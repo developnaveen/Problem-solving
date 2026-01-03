d2e8bd1dbf2a06b2ff58eede12b0b71d0173dc74
developnaveen <naveenradha2907@gmail.com>
2026-01-03 19:08:20 +0530

    comp[leted

---
+++ sampleservlet/src/main/java/com/example/sampleservlet/util/JwtUtil.java
@@ -0,0 +1,13 @@
+package com.example.sampleservlet.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "";
+    private static final long EXPIRATION_TIME = 60 * 60 * 1000;
+
+
+
+}
